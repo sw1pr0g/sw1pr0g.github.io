64a6ad8ab0406f422851bababa93dc757cf33e36
Alex Poryadin <poryadin_alex@mail.ru>
2024-10-30 23:05:44 +0300

    add static generation

--- makefile
+++ makefile
@@ -8,4 +8,4 @@ clean:
 	@-rm -f web/sw1site
 #	@PORT=$$(yq e '.http.port' config/config.yml) && echo "port: $$PORT" && \
 #    	PID=$$(lsof -t -i:$$PORT) && echo "PID: $$PID\n successfully cleaned!" || echo "No process found on port $$PORT" && \
-#    	if [ -n "$$PID" ]; then kill $$PID; else echo "No process to kill"; fi
+#    	if [ -n "$$PID" ]; then kill $$PID; else echo "No process to kill";ч fi
--- src/sw1pr0g/main.go
+++ src/sw1pr0g/main.go
@@ -3,13 +3,50 @@ package main
 import (
 	"fmt"
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
-	"github.com/sw1pr0g/sw1pr0g.github.io/src/sw1pr0g/components"
+	"log"
 	"net/http"
+	"os"
 )
 
+type Home struct {
+	app.Compo
+}
+
+func (h *Home) Render() app.UI {
+	return app.Div().Body(
+		app.P().
+			Text("sw1prog's home.. maybe you were waiting for the design, but I don't care about that"),
+		app.P().Body(
+			app.Text("i have a "),
+			app.A().
+				Text("telegram channel").
+				Href("https://t.me/sw1pr0g_channel").
+				Target("_blank"),
+		),
+		app.P().Body(
+			app.Text("check out my "),
+			app.A().
+				Text("github").
+				Href("https://github.com/sw1pr0g").
+				Target("_blank"),
+		),
+	)
+}
+
 func main() {
-	app.Route("/", &components.Home{})
+	app.Route("/", &Home{})
 	app.RunWhenOnBrowser()
+
+	err := app.GenerateStaticWebsite("docs", &app.Handler{
+		Author:      "Alex Poryadin",
+		Description: "sw1pr0g personal page",
+		Name:        "sw1pr0g",
+		Title:       "sw1pr0g",
+	})
+	if err != nil {
+		log.Fatalf("Failed to generate static website: %v", err)
+		os.Exit(1)
+	}
 
 	h := app.Handler{
 		Author:      "Alex Poryadin",
